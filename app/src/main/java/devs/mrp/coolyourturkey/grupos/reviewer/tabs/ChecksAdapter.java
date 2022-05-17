7528f38380abf0c7aed6cd06049c8e7b29c8602c
Miguel Rietz <miguel.rietz@netcentric.biz>
2022-05-17 12:33:29 +0200

    setting common groups

--- app/src/main/java/devs/mrp/coolyourturkey/grupos/reviewer/tabs/ChecksAdapter.java
+++ app/src/main/java/devs/mrp/coolyourturkey/grupos/reviewer/tabs/ChecksAdapter.java
@@ -1,4 +1,67 @@
 package devs.mrp.coolyourturkey.grupos.reviewer.tabs;
 
-public class ChecksAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import devs.mrp.coolyourturkey.R;
+import devs.mrp.coolyourturkey.databaseroom.elementtogroup.ElementToGroup;
+import devs.mrp.coolyourturkey.plantillas.FeedbackListener;
+import devs.mrp.coolyourturkey.plantillas.Feedbacker;
+
+public class ChecksAdapter extends RecyclerView.Adapter<ChecksAdapter.CheckViewHolder> implements Feedbacker<ElementToGroup> {
+
+    private Context mContext;
+    private Integer mGroupId;
+
+    public ChecksAdapter(Context context, Integer thisGroupId) {
+        this.mContext = context;
+        this.mGroupId = thisGroupId;
+    }
+
+    @NonNull
+    @Override
+    public ChecksAdapter.CheckViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChecksAdapter.CheckViewHolder holder, int position) {
+
+    }
+
+    protected static class CheckViewHolder extends RecyclerView.ViewHolder {
+
+        public Switch switchView;
+        public TextView textView;
+        public Integer checkId;
+
+        public CheckViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+            switchView = itemView.findViewById(R.id.switch1);
+            checkId = null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void giveFeedback(int tipo, ElementToGroup feedback) {
+
+    }
+
+    @Override
+    public void addFeedbackListener(FeedbackListener<ElementToGroup> listener) {
+
+    }
 }
--- app/src/main/java/devs/mrp/coolyourturkey/grupos/reviewer/tabs/ChecksTabFragment.java
+++ app/src/main/java/devs/mrp/coolyourturkey/grupos/reviewer/tabs/ChecksTabFragment.java
@@ -1,11 +1,27 @@
 package devs.mrp.coolyourturkey.grupos.reviewer.tabs;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import devs.mrp.coolyourturkey.R;
+import devs.mrp.coolyourturkey.databaseroom.checktimeblocks.CheckTimeBlock;
+import devs.mrp.coolyourturkey.databaseroom.checktimeblocks.CheckTimeBlockViewModel;
 
 public class ChecksTabFragment extends Fragment {
 
@@ -16,9 +32,67 @@ public class ChecksTabFragment extends Fragment {
     private Context mContext;
     private ViewModelProvider.Factory viewModelFactory;
     private Integer mGroupId;
+    private Type type;
+
+    private ChecksAdapter mChecksAdapter;
+
+    private FutureTask<CheckTimeBlock> fillAdapterTask;
+    private ExecutorService executor = Executors.newFixedThreadPool(1);
+
+    public ChecksTabFragment(Type type, Integer groupId) {
+        super();
+        this.type = type;
+        this.mGroupId = groupId;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewModelFactory = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication());
+
+        mContext = getActivity();
+        mainHandler = new Handler(mContext.getMainLooper());
+
+        if (savedInstanceState != null && !savedInstanceState.isEmpty()) {
+            mGroupId = savedInstanceState.getInt(KEY_BUNDLE_ID_ACTUAL);
+        }
+
+        View v = inflater.inflate(R.layout.fragment_single_recycler, container, false);
+        mRecyclerView = v.findViewById(R.id.recyclerView);
+
+        mChecksAdapter = new ChecksAdapter(mContext, mGroupId);
+
+        return v;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(KEY_BUNDLE_ID_ACTUAL, mGroupId);
+        super.onSaveInstanceState(outState);
+    }
 
     public enum Type {
         POSITIVE, NEGATIVE;
+    }
+
+    private LiveData<List<CheckTimeBlock>> getChecks(CheckTimeBlockViewModel model) {
+        if (Type.POSITIVE.equals(this.type)) {
+            return model.findAllTimeBlocks();
+        }
+        return null;
+    }
+
+    public Integer getGroupId() {
+        if (mGroupId == null) {
+            return -1;
+        }
+        return mGroupId;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        executor.execute(fillAdapterTask);
     }
 
 }
