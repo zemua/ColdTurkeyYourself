3cb47afe7bdd32fd20b011e1e63e33b9e7c1e531
Miguel Rietz <miguel.rietz@netcentric.biz>
2022-10-12 17:45:12 +0200

    waiting time to remove app from group

--- app/src/main/java/devs/mrp/coolyourturkey/comun/DialogWithDelayPresenter.java
+++ app/src/main/java/devs/mrp/coolyourturkey/comun/DialogWithDelayPresenter.java
@@ -1,10 +1,13 @@
 package devs.mrp.coolyourturkey.comun;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public interface DialogWithDelayPresenter {
     public void setListener(String requestKey, Consumer<Boolean> consumer);
+    public void setListener(String requestKey, BiConsumer<Boolean, Object> consumer);
     public void showDialog(String requestKey, String titulo, String mensaje, int iconoResId);
     public void showDialog(String requestKey, String titulo, String mensaje);
     public void showDialog(String requestKey);
+    public void showDialog(String requestKey, Object data);
 }
--- app/src/main/java/devs/mrp/coolyourturkey/comun/impl/DialogWithDelayPresenterImpl.java
+++ app/src/main/java/devs/mrp/coolyourturkey/comun/impl/DialogWithDelayPresenterImpl.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import devs.mrp.coolyourturkey.R;
@@ -20,6 +21,7 @@ public class DialogWithDelayPresenterImpl implements DialogWithDelayPresenter {
     private Fragment fragment;
     private DialogWithDelayAndFragmentResponseFactory dialogFactory;
     private int mDelaySeconds;
+    private Object data = null;
 
     public DialogWithDelayPresenterImpl(Fragment fragment, DialogWithDelayAndFragmentResponseFactory factory, int delaySeconds) {
         fragmentManager = fragment.getActivity().getSupportFragmentManager();
@@ -31,7 +33,13 @@ public class DialogWithDelayPresenterImpl implements DialogWithDelayPresenter {
     @Override
     public void setListener(String requestKey, Consumer<Boolean> consumer) {
         FragmentResultListener fragmentResultListener = (key, bundle) -> consumer.accept(bundle.getBoolean(resultKey));
-        fragmentManager.setFragmentResultListener(requestKey, fragment.getViewLifecycleOwner(), fragmentResultListener);
+        fragmentManager.setFragmentResultListener(requestKey, fragment, fragmentResultListener);
+    }
+
+    @Override
+    public void setListener(String requestKey, BiConsumer<Boolean, Object> consumer) {
+        FragmentResultListener fragmentResultListener = (key, bundle) -> consumer.accept(bundle.getBoolean(resultKey), data);
+        fragmentManager.setFragmentResultListener(requestKey, fragment, fragmentResultListener);
     }
 
     @Override
@@ -50,5 +58,11 @@ public class DialogWithDelayPresenterImpl implements DialogWithDelayPresenter {
     @Override
     public void showDialog(String requestKey) {
         showDialog(requestKey, fragment.getString(R.string.recapacita), fragment.getString(R.string.estas_seguro_que_debes_hacer_esto));
+    }
+
+    @Override
+    public void showDialog(String requestKey, Object data) {
+        this.data = data;
+        showDialog(requestKey);
     }
 }
