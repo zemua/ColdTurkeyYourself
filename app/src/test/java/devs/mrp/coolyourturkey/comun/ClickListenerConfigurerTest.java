894c5fec895ccc02b53cb4294dac34d048537c62
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-05-20 00:29:17 +0200

    testing view configurer builder

---
+++ app/src/test/java/devs/mrp/coolyourturkey/comun/ClickListenerConfigurerTest.java
@@ -0,0 +1,14 @@
+package devs.mrp.coolyourturkey.comun;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class ClickListenerConfigurerTest {
+
+    @Test
+    void test() {
+        fail();
+    }
+
+}
---
+++ app/src/test/java/devs/mrp/coolyourturkey/configuracion/modules/builder/ClickListenerConfigurerBuilderTest.java
@@ -0,0 +1,14 @@
+package devs.mrp.coolyourturkey.configuracion.modules.builder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClickListenerConfigurerBuilderTest {
+
+    @Test
+    void test() {
+        fail();
+    }
+
+}
