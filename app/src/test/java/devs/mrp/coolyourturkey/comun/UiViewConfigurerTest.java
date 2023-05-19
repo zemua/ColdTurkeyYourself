23018f2f00816c75c07d40f6d96ae0aab49ba406
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-05-20 01:38:35 +0200

    testing view configurer

--- app/src/test/java/devs/mrp/coolyourturkey/comun/UiViewConfigurerTest.java
+++ app/src/test/java/devs/mrp/coolyourturkey/comun/UiViewConfigurerTest.java
@@ -1,75 +1,85 @@
 package devs.mrp.coolyourturkey.comun;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import android.view.View;
 import android.widget.Switch;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 import devs.mrp.coolyourturkey.configuracion.MisPreferencias;
 import devs.mrp.coolyourturkey.configuracion.PreferencesBooleanEnum;
-import devs.mrp.coolyourturkey.configuracion.modules.builder.ClickListenerConfigurerBuilder;
-import devs.mrp.coolyourturkey.configuracion.modules.builder.impl.SwitchClickListenerBuilder;
+import devs.mrp.coolyourturkey.configuracion.modules.beans.PreferencesSwitchConfigurer;
 
 @ExtendWith(MockitoExtension.class)
 class UiViewConfigurerTest {
 
-    private ClickListenerConfigurerBuilder<Switch, MisPreferencias, PreferencesBooleanEnum> clickListenerConfigurerBuilder;
+    private UiViewConfigurer<Switch, PreferencesBooleanEnum> configurer;
 
     @Mock
-    MisPreferencias preferencias;
+    MisPreferencias prefs;
     @Mock
-    DialogWithDelayPresenter dialogWithDelayPresenter;
+    ClickListenerConfigurer<Switch, PreferencesBooleanEnum> listenerFactory;
     @Mock
-    Runnable onStateChangeAction;
+    View.OnClickListener listener;
+    @Mock
+    View parent;
+    Integer resourceId = 123;
+    PreferencesBooleanEnum identifier;
+    List<Supplier<Boolean>> requiredFalseEnablers;
+    List<Supplier<Boolean>> requiredTrueEnablers;
+    @Mock
+    ViewDisabler viewDisabler;
+    @Mock
+    Switch aSwitch;
 
     @BeforeEach
     void setup() {
-        clickListenerConfigurerBuilder = new SwitchClickListenerBuilder();
-        clickListenerConfigurerBuilder.preferencias(preferencias);
-        clickListenerConfigurerBuilder.dialogWithDelayPresenter(dialogWithDelayPresenter);
+        identifier = PreferencesBooleanEnum.LOCKDOWN_POSITIVE_DECREASE;
+        requiredTrueEnablers = Arrays.asList(() -> true, ()-> false);
+        requiredFalseEnablers = Arrays.asList(()->false, ()->true, () -> false, ()-> false);
+        configurer = new PreferencesSwitchConfigurer(prefs, listenerFactory, parent, resourceId, identifier, requiredFalseEnablers, requiredTrueEnablers, viewDisabler);
+        when(parent.findViewById(123)).thenReturn(aSwitch);
+        when(listenerFactory.getListener(PreferencesBooleanEnum.LOCKDOWN_POSITIVE_DECREASE)).thenReturn(listener);
+        when(prefs.getBoolean(PreferencesBooleanEnum.LOCKDOWN_POSITIVE_DECREASE, PreferencesBooleanEnum.LOCKDOWN_POSITIVE_DECREASE.getDefaultState())).thenReturn(true);
     }
 
     @Test
-    void testConfigureCorrectly() {
-        ClickListenerConfigurer<Switch, PreferencesBooleanEnum> result = clickListenerConfigurerBuilder.build();
-        assertNotNull(result);
-    }
+    void test() {
+        Optional<Switch> viewOptional = configurer.buildElement();
+        assertTrue(viewOptional.isPresent());
 
-    @Test
-    void testConfigureWithNullPreferences() {
-        assertThrows(RuntimeException.class, () -> clickListenerConfigurerBuilder.preferencias(null).build());
-    }
+        ArgumentCaptor<List<Supplier<Boolean>>> listCaptor = ArgumentCaptor.forClass(List.class);
+        verify(viewDisabler, times(1)).addViewConditions(ArgumentMatchers.eq(aSwitch), listCaptor.capture());
+        List<Supplier<Boolean>> list = listCaptor.getValue();
+        assertEquals(6, list.size());
+        // true enablers are kept as is
+        assertTrue(list.get(0).get());
+        assertFalse(list.get(1).get());
+        // false enablers are inverted
+        assertTrue(list.get(2).get());
+        assertFalse(list.get(3).get());
+        assertTrue(list.get(4).get());
+        assertTrue(list.get(5).get());
 
-    @Test
-    void testConfigureWithNullDialog() {
-        assertThrows(RuntimeException.class, () -> clickListenerConfigurerBuilder.dialogWithDelayPresenter(null).build());
-    }
-
-    @Test
-    void testKeepsChangeAction() throws NoSuchFieldException, IllegalAccessException {
-        ClickListenerConfigurer<Switch, PreferencesBooleanEnum> result = clickListenerConfigurerBuilder.onStateChangeAction(onStateChangeAction).build();
-        Field field = result.getClass().getDeclaredField("doOnChangeAction");
-        field.setAccessible(true);
-        Runnable value = (Runnable) field.get(result);
-        assertEquals(onStateChangeAction, value);
-    }
-
-    @Test
-    void createsEmptyChangeActionIfNotProvided() throws NoSuchFieldException, IllegalAccessException {
-        ClickListenerConfigurer<Switch, PreferencesBooleanEnum> result = clickListenerConfigurerBuilder.onStateChangeAction(null).build();
-        Field field = result.getClass().getDeclaredField("doOnChangeAction");
-        field.setAccessible(true);
-        Runnable value = (Runnable) field.get(result);
-        assertNotNull(value);
+        verify(aSwitch, times(1)).setOnClickListener(listener);
+        verify(aSwitch, times(1)).setChecked(true);
     }
 
 }
--- app/src/test/java/devs/mrp/coolyourturkey/configuracion/modules/builder/ClickListenerConfigurerBuilderTest.java
+++ app/src/test/java/devs/mrp/coolyourturkey/configuracion/modules/builder/ClickListenerConfigurerBuilderTest.java
@@ -1,14 +1,76 @@
 package devs.mrp.coolyourturkey.configuracion.modules.builder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import android.widget.Switch;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Field;
+
+import devs.mrp.coolyourturkey.comun.ClickListenerConfigurer;
+import devs.mrp.coolyourturkey.comun.DialogWithDelayPresenter;
+import devs.mrp.coolyourturkey.configuracion.MisPreferencias;
+import devs.mrp.coolyourturkey.configuracion.PreferencesBooleanEnum;
+import devs.mrp.coolyourturkey.configuracion.modules.builder.impl.SwitchClickListenerBuilder;
+
+@ExtendWith(MockitoExtension.class)
 class ClickListenerConfigurerBuilderTest {
 
+    private ClickListenerConfigurerBuilder<Switch, MisPreferencias, PreferencesBooleanEnum> clickListenerConfigurerBuilder;
+
+    @Mock
+    MisPreferencias preferencias;
+    @Mock
+    DialogWithDelayPresenter dialogWithDelayPresenter;
+    @Mock
+    Runnable onStateChangeAction;
+
+    @BeforeEach
+    void setup() {
+        clickListenerConfigurerBuilder = new SwitchClickListenerBuilder();
+        clickListenerConfigurerBuilder.preferencias(preferencias);
+        clickListenerConfigurerBuilder.dialogWithDelayPresenter(dialogWithDelayPresenter);
+    }
+
     @Test
-    void test() {
-        fail();
+    void testConfigureCorrectly() {
+        ClickListenerConfigurer<Switch, PreferencesBooleanEnum> result = clickListenerConfigurerBuilder.build();
+        assertNotNull(result);
+    }
+
+    @Test
+    void testConfigureWithNullPreferences() {
+        assertThrows(RuntimeException.class, () -> clickListenerConfigurerBuilder.preferencias(null).build());
+    }
+
+    @Test
+    void testConfigureWithNullDialog() {
+        assertThrows(RuntimeException.class, () -> clickListenerConfigurerBuilder.dialogWithDelayPresenter(null).build());
+    }
+
+    @Test
+    void testKeepsChangeAction() throws NoSuchFieldException, IllegalAccessException {
+        ClickListenerConfigurer<Switch, PreferencesBooleanEnum> result = clickListenerConfigurerBuilder.onStateChangeAction(onStateChangeAction).build();
+        Field field = result.getClass().getDeclaredField("doOnChangeAction");
+        field.setAccessible(true);
+        Runnable value = (Runnable) field.get(result);
+        assertEquals(onStateChangeAction, value);
+    }
+
+    @Test
+    void createsEmptyChangeActionIfNotProvided() throws NoSuchFieldException, IllegalAccessException {
+        ClickListenerConfigurer<Switch, PreferencesBooleanEnum> result = clickListenerConfigurerBuilder.onStateChangeAction(null).build();
+        Field field = result.getClass().getDeclaredField("doOnChangeAction");
+        field.setAccessible(true);
+        Runnable value = (Runnable) field.get(result);
+        assertNotNull(value);
     }
 
 }
