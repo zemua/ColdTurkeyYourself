8f7342926595975296dd6c2a786121918f1ad220
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-05-27 20:00:37 +0200

    adapt to api 33

--- app/src/main/java/devs/mrp/coolyourturkey/comun/NotificadorWithIntent.java
+++ app/src/main/java/devs/mrp/coolyourturkey/comun/NotificadorWithIntent.java
@@ -13,7 +13,7 @@ import androidx.core.app.NotificationCompat;
 public class NotificadorWithIntent {
 
     public static Notification notifyWithIntentAndCustomSound(Integer iconResourceId, String title, String content, Context context, Intent intent, String channelId, int requestCode, Uri soundUri){
-        PendingIntent contentIntent = PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         Resources res = context.getResources();
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId);
         builder.setContentIntent(contentIntent)
@@ -27,7 +27,7 @@ public class NotificadorWithIntent {
     }
 
     public static Notification notifyWithIntent(Integer iconResourceId, String title, String content, Context context, Intent intent, String channelId, int requestCode){
-        PendingIntent contentIntent = PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         Resources res = context.getResources();
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId);
         builder.setContentIntent(contentIntent)
